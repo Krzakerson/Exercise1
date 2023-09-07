e1df3d9aceab01dcfa58b269b7a2ae4ef16c21c9
Jack Soplica <82238300+SoplicaIndustries@users.noreply.github.com>
2023-09-07 15:59:06 +0200

    Image from folder

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,174 +29,185 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.picBox = new System.Windows.Forms.PictureBox();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnPrev = new System.Windows.Forms.Button();
-            this.btnNext = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.menu1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.menu2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.menuStrip1.SuspendLayout();
-            this.SuspendLayout();
+            openFileDialog1 = new OpenFileDialog();
+            panel1 = new Panel();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            picBox = new PictureBox();
+            progressBar1 = new ProgressBar();
+            btnSave = new Button();
+            btnResize = new Button();
+            btnPrev = new Button();
+            btnNext = new Button();
+            pictureBox1 = new PictureBox();
+            menuStrip1 = new MenuStrip();
+            menu1 = new ToolStripMenuItem();
+            menu2 = new ToolStripMenuItem();
+            btnEdit = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileName = "openFileDialog1";
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.panel1.Controls.Add(this.pictureBox3);
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.picBox);
-            this.panel1.Controls.Add(this.progressBar1);
-            this.panel1.Controls.Add(this.btnSave);
-            this.panel1.Controls.Add(this.btnEdit);
-            this.panel1.Controls.Add(this.btnPrev);
-            this.panel1.Controls.Add(this.btnNext);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.menuStrip1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 450);
-            this.panel1.TabIndex = 0;
+            panel1.BackColor = Color.WhiteSmoke;
+            panel1.Controls.Add(btnEdit);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(picBox);
+            panel1.Controls.Add(progressBar1);
+            panel1.Controls.Add(btnSave);
+            panel1.Controls.Add(btnResize);
+            panel1.Controls.Add(btnPrev);
+            panel1.Controls.Add(btnNext);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(menuStrip1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 450);
+            panel1.TabIndex = 0;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(62, 54);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(444, 286);
-            this.pictureBox3.TabIndex = 9;
-            this.pictureBox3.TabStop = false;
+            pictureBox3.Location = new Point(62, 54);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(444, 286);
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(689, 29);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(51, 52);
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(689, 29);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(51, 52);
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // picBox
             // 
-            this.picBox.Image = ((System.Drawing.Image)(resources.GetObject("picBox.Image")));
-            this.picBox.Location = new System.Drawing.Point(749, 29);
-            this.picBox.Name = "picBox";
-            this.picBox.Size = new System.Drawing.Size(51, 52);
-            this.picBox.TabIndex = 6;
-            this.picBox.TabStop = false;
-            this.picBox.Click += new System.EventHandler(this.picBox_Click_1);
+            picBox.Image = (Image)resources.GetObject("picBox.Image");
+            picBox.Location = new Point(749, 29);
+            picBox.Name = "picBox";
+            picBox.Size = new Size(51, 52);
+            picBox.TabIndex = 6;
+            picBox.TabStop = false;
+            picBox.Click += picBox_Click_1;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(62, 390);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 5;
+            progressBar1.Location = new Point(62, 390);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(100, 23);
+            progressBar1.TabIndex = 5;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(548, 226);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(102, 27);
-            this.btnSave.TabIndex = 4;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
+            btnSave.Location = new Point(548, 313);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(102, 27);
+            btnSave.TabIndex = 4;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
             // 
-            // btnEdit
+            // btnResize
             // 
-            this.btnEdit.Location = new System.Drawing.Point(548, 162);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(102, 27);
-            this.btnEdit.TabIndex = 3;
-            this.btnEdit.Text = "Edit";
-            this.btnEdit.UseVisualStyleBackColor = true;
+            btnResize.Location = new Point(548, 174);
+            btnResize.Name = "btnResize";
+            btnResize.Size = new Size(102, 27);
+            btnResize.TabIndex = 3;
+            btnResize.Text = "Resize";
+            btnResize.UseVisualStyleBackColor = true;
+            btnResize.Click += btnResize_Click;
             // 
             // btnPrev
             // 
-            this.btnPrev.Location = new System.Drawing.Point(548, 107);
-            this.btnPrev.Name = "btnPrev";
-            this.btnPrev.Size = new System.Drawing.Size(102, 27);
-            this.btnPrev.TabIndex = 2;
-            this.btnPrev.Text = "Previous image";
-            this.btnPrev.UseVisualStyleBackColor = true;
+            btnPrev.Location = new Point(548, 116);
+            btnPrev.Name = "btnPrev";
+            btnPrev.Size = new Size(102, 27);
+            btnPrev.TabIndex = 2;
+            btnPrev.Text = "Previous image";
+            btnPrev.UseVisualStyleBackColor = true;
+            btnPrev.Click += btnPrev_Click;
             // 
             // btnNext
             // 
-            this.btnNext.Location = new System.Drawing.Point(548, 54);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(102, 27);
-            this.btnNext.TabIndex = 1;
-            this.btnNext.Text = "Next image";
-            this.btnNext.UseVisualStyleBackColor = true;
+            btnNext.Location = new Point(548, 54);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(102, 27);
+            btnNext.TabIndex = 1;
+            btnNext.Text = "Next image";
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += btnNext_Click;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(62, 54);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(444, 286);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            pictureBox1.Location = new Point(62, 54);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(444, 286);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // menuStrip1
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.menu1,
-            this.menu2});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 8;
-            this.menuStrip1.Text = "menuStrip1";
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menu1, menu2 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 8;
+            menuStrip1.Text = "menuStrip1";
             // 
             // menu1
             // 
-            this.menu1.Name = "menu1";
-            this.menu1.Size = new System.Drawing.Size(84, 20);
-            this.menu1.Text = "Open Folder";
-            this.menu1.Click += new System.EventHandler(this.menu1_Click);
+            menu1.Name = "menu1";
+            menu1.Size = new Size(84, 20);
+            menu1.Text = "Open Folder";
+            menu1.Click += menu1_Click;
             // 
             // menu2
             // 
-            this.menu2.Name = "menu2";
-            this.menu2.Size = new System.Drawing.Size(69, 20);
-            this.menu2.Text = "Open File";
-            this.menu2.Click += new System.EventHandler(this.menu2_Click);
+            menu2.Name = "menu2";
+            menu2.Size = new Size(69, 20);
+            menu2.Text = "Open File";
+            menu2.Click += menu2_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(548, 239);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(102, 27);
+            btnEdit.TabIndex = 10;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
@@ -205,7 +216,7 @@
         private Panel panel1;
         private ProgressBar progressBar1;
         private Button btnSave;
-        private Button btnEdit;
+        private Button btnResize;
         private Button btnPrev;
         private Button btnNext;
         private PictureBox pictureBox1;
@@ -215,5 +226,6 @@
         private ToolStripMenuItem menu1;
         private ToolStripMenuItem menu2;
         private PictureBox pictureBox3;
+        private Button btnEdit;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FirstExercise4Class
 {
@@ -8,33 +9,34 @@ namespace FirstExercise4Class
         public Form1()
         {
             InitializeComponent();
-            
+
 
         }
-       
-        
-
+        string folderpath = "";
+        string[] files;
+        int n;
+        int manyoffile;
         private void picBox_Click_1(object sender, EventArgs e)
         {
-           
-                panel1.BackColor = Color.WhiteSmoke;
-               
-            
+
+            panel1.BackColor = Color.WhiteSmoke;
+
+
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
             panel1.BackColor = Color.DimGray;
-            
+
         }
 
         private void menu2_Click(object sender, EventArgs e)
         {
-            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 string ext = Path.GetExtension(openFileDialog1.FileName);
 
-                if(ext == ".jpg" || ext ==  ".png" || ext == ".jpeg")
+                if (ext == ".jpg" || ext == ".png" || ext == ".jpeg")
                 {
                     pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
                     pictureBox3.Image = new Bitmap(openFileDialog1.FileName);
@@ -48,17 +50,79 @@ namespace FirstExercise4Class
 
         private void menu1_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();    
-            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
             {
-                string folder = folderBrowserDialog.SelectedPath;
-                string[] files = Directory.GetFiles(folder);
+                folderpath = folderBrowserDialog.SelectedPath;
+                files = Directory.GetFiles(folderpath);
 
 
-                int manyoffile = files.Length;
+                manyoffile = files.Length;
 
-                MessageBox.Show(manyoffile.ToString());
+                n = 0;
+
+                string fileext = Path.GetExtension(files[n]);
+
+                if (fileext == ".jpg" || fileext == ".png" || fileext == ".jpeg")
+                {
+                    pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+                    pictureBox3.Image = new Bitmap(files[n]);
+                }
+                else
+                {
+                    MessageBox.Show("Format pliku nie jest obs³ugiwany");
+                }
             }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n++;
+                string fileext = Path.GetExtension(files[n]);
+                if (fileext == ".jpg" || fileext == ".png" || fileext == ".jpeg")
+                {
+                    pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+                    pictureBox3.Image = new Bitmap(files[n]);
+                }
+                else
+                {
+                    MessageBox.Show("Format pliku nie jest obs³ugiwany");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n--;
+                string fileext = Path.GetExtension(files[n]);
+                if (fileext == ".jpg" || fileext == ".png" || fileext == ".jpeg")
+                {
+                    pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+                    pictureBox3.Image = new Bitmap(files[n]);
+                }
+                else
+                {
+                    MessageBox.Show("Format pliku nie jest obs³ugiwany");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
         }
     }
 }
