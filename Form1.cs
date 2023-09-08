05bcedf25702b85fef1d78c84f6f7805ce9ae1d7
Jack Soplica <82238300+SoplicaIndustries@users.noreply.github.com>
2023-09-08 09:39:50 +0200

    save update and bitmap convert

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,8 @@ namespace FirstExercise4Class
 
 
         }
+        bool IsDrawing = false;
+        Bitmap bitmap;
         string folderpath = "";
         string[] files;
         int n;
@@ -25,11 +27,11 @@ namespace FirstExercise4Class
         {
             panel1.BackColor = Color.DimGray;
         }
-       
 
-        
 
-       
+
+
+
 
         private void btnNext_Click(object sender, EventArgs e)
         {
@@ -83,22 +85,56 @@ namespace FirstExercise4Class
 
         private void pictureBox4_Click(object sender, EventArgs e)
         {
-
+            if(pictureBox3.Image != null)
+            {
+                var pen = new Pen(Color.Red, 1);
+                var bmp = (Bitmap)pictureBox3.Image;
+            }
+            else
+            {
+                MessageBox.Show("First load image");
+            }
+         
         }
 
         private void pictureBox5_Click(object sender, EventArgs e)
         {
+            if (pictureBox3.Image != null)
+            {
+                var pen = new Pen(Color.Green, 1);
+                var bmp = (Bitmap)pictureBox3.Image;
+            }
+            else
+            {
+                MessageBox.Show("First load image");
+            }
 
         }
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
-            
+            if (pictureBox3.Image != null)
+            {
+                var pen = new Pen(Color.White, 1);
+                var bmp = (Bitmap)pictureBox3.Image;
+            }
+            else
+            {
+                MessageBox.Show("First load image");
+            }
         }
 
         private void pictureBox7_Click(object sender, EventArgs e)
         {
-
+            if (pictureBox3.Image != null)
+            {
+                var pen = new Pen(Color.Black, 1);
+                var bmp = (Bitmap)pictureBox3.Image;
+            }
+            else
+            {
+                MessageBox.Show("First load image");
+            }
         }
 
         private void menu1_Click_1(object sender, EventArgs e)
@@ -106,6 +142,11 @@ namespace FirstExercise4Class
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
             if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
             {
+                btnNext.Enabled = true;
+                btnPrev.Enabled = true;
+                btnResize.Enabled = true;
+                btnEdit.Enabled = true;
+                btnSave.Enabled = true;
                 folderpath = folderBrowserDialog.SelectedPath;
                 files = Directory.GetFiles(folderpath);
 
@@ -132,6 +173,9 @@ namespace FirstExercise4Class
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
+                btnResize.Enabled = true;
+                btnEdit.Enabled = true;
+                btnSave.Enabled = true;
                 string ext = Path.GetExtension(openFileDialog1.FileName);
 
                 if (ext == ".jpg" || ext == ".png" || ext == ".jpeg")
@@ -146,7 +190,7 @@ namespace FirstExercise4Class
             }
         }
 
-       
+
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
@@ -155,31 +199,31 @@ namespace FirstExercise4Class
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-                if(pictureBox3 != null)
+            if (pictureBox3 != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-                    if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-                    {
 
-                  
 
-                    string path = folderBrowserDialog.SelectedPath;
 
-                        pictureBox3.Image.Save(path, ImageFormat.Jpeg);
-                    }
+
+
+                    pictureBox3.Image.Save(saveFileDialog.FileName);
                 }
+            }
             else
             {
                 MessageBox.Show("There is not image to be save");
             }
-            
-               
-            
-           
 
-            
+
+
+
+
+
         }
 
-      
+
     }
 }
