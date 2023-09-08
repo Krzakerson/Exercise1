8bf1fc79b824b0f9e00a37d91ecd01f08b3490b4
Krzakerson <92225516+Krzakerson@users.noreply.github.com>
2023-09-08 14:32:36 +0200

    concept change

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,256 +29,187 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            openFileDialog1 = new OpenFileDialog();
-            panel1 = new Panel();
-            panel2 = new Panel();
-            pictureBox7 = new PictureBox();
-            pictureBox6 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            btnEdit = new Button();
-            pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            picture = new PictureBox();
-            btnSave = new Button();
-            btnResize = new Button();
-            btnPrev = new Button();
-            btnNext = new Button();
-            pictureBox1 = new PictureBox();
-            menuStrip1 = new MenuStrip();
-            menu1 = new ToolStripMenuItem();
-            menu2 = new ToolStripMenuItem();
-            colorDialog1 = new ColorDialog();
-            panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)picture).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            menuStrip1.SuspendLayout();
-            SuspendLayout();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
             // 
             // openFileDialog1
             // 
-            openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // panel1
             // 
-            panel1.BackColor = Color.WhiteSmoke;
-            panel1.Controls.Add(panel2);
-            panel1.Controls.Add(btnEdit);
-            panel1.Controls.Add(pictureBox3);
-            panel1.Controls.Add(pictureBox2);
-            panel1.Controls.Add(picture);
-            panel1.Controls.Add(btnSave);
-            panel1.Controls.Add(btnResize);
-            panel1.Controls.Add(btnPrev);
-            panel1.Controls.Add(btnNext);
-            panel1.Controls.Add(pictureBox1);
-            panel1.Controls.Add(menuStrip1);
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(800, 450);
-            panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.Black;
-            panel2.Controls.Add(pictureBox7);
-            panel2.Controls.Add(pictureBox6);
-            panel2.Controls.Add(pictureBox5);
-            panel2.Controls.Add(pictureBox4);
-            panel2.Location = new Point(242, 390);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(264, 48);
-            panel2.TabIndex = 11;
-            // 
-            // pictureBox7
-            // 
-            pictureBox7.BackColor = Color.Black;
-            pictureBox7.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox7.Location = new Point(215, 3);
-            pictureBox7.Name = "pictureBox7";
-            pictureBox7.Size = new Size(36, 42);
-            pictureBox7.TabIndex = 3;
-            pictureBox7.TabStop = false;
-            pictureBox7.Click += pictureBox7_Click;
-            // 
-            // pictureBox6
-            // 
-            pictureBox6.BackColor = Color.White;
-            pictureBox6.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox6.Location = new Point(149, 3);
-            pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(36, 42);
-            pictureBox6.TabIndex = 2;
-            pictureBox6.TabStop = false;
-            pictureBox6.Click += pictureBox6_Click;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.BackColor = Color.Green;
-            pictureBox5.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox5.Location = new Point(77, 3);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(36, 42);
-            pictureBox5.TabIndex = 1;
-            pictureBox5.TabStop = false;
-            pictureBox5.Click += pictureBox5_Click;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.BackColor = Color.Red;
-            pictureBox4.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox4.Location = new Point(14, 3);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(36, 42);
-            pictureBox4.TabIndex = 0;
-            pictureBox4.TabStop = false;
-            pictureBox4.Click += pictureBox4_Click;
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.picture);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnResize);
+            this.panel1.Controls.Add(this.btnPrev);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
             // 
             // btnEdit
             // 
-            btnEdit.Enabled = false;
-            btnEdit.Location = new Point(548, 239);
-            btnEdit.Name = "btnEdit";
-            btnEdit.Size = new Size(102, 27);
-            btnEdit.TabIndex = 10;
-            btnEdit.Text = "Edit";
-            btnEdit.UseVisualStyleBackColor = true;
-            btnEdit.Click += btnEdit_Click;
+            this.btnEdit.Enabled = false;
+            this.btnEdit.Location = new System.Drawing.Point(548, 239);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(102, 27);
+            this.btnEdit.TabIndex = 10;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // pictureBox3
             // 
-            pictureBox3.Location = new Point(62, 54);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(444, 286);
-            pictureBox3.TabIndex = 9;
-            pictureBox3.TabStop = false;
+            this.pictureBox3.Location = new System.Drawing.Point(62, 54);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(444, 286);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(689, 29);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(51, 52);
-            pictureBox2.TabIndex = 7;
-            pictureBox2.TabStop = false;
-            pictureBox2.Click += pictureBox2_Click;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(689, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 52);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.picBox_Click);
             // 
             // picture
             // 
-            picture.Image = (Image)resources.GetObject("picture.Image");
-            picture.Location = new Point(749, 29);
-            picture.Name = "picture";
-            picture.Size = new Size(51, 52);
-            picture.TabIndex = 6;
-            picture.TabStop = false;
-            picture.Click += picBox_Click;
+            this.picture.Image = ((System.Drawing.Image)(resources.GetObject("picture.Image")));
+            this.picture.Location = new System.Drawing.Point(749, 29);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(51, 52);
+            this.picture.TabIndex = 6;
+            this.picture.TabStop = false;
+            this.picture.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // btnSave
             // 
-            btnSave.Enabled = false;
-            btnSave.Location = new Point(548, 313);
-            btnSave.Name = "btnSave";
-            btnSave.Size = new Size(102, 27);
-            btnSave.TabIndex = 4;
-            btnSave.Text = "Save";
-            btnSave.UseVisualStyleBackColor = true;
-            btnSave.Click += btnSave_Click;
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(548, 313);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(102, 27);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnResize
             // 
-            btnResize.Enabled = false;
-            btnResize.Location = new Point(548, 174);
-            btnResize.Name = "btnResize";
-            btnResize.Size = new Size(102, 27);
-            btnResize.TabIndex = 3;
-            btnResize.Text = "Resize";
-            btnResize.UseVisualStyleBackColor = true;
-            btnResize.Click += btnResize_Click;
+            this.btnResize.Enabled = false;
+            this.btnResize.Location = new System.Drawing.Point(548, 174);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(102, 27);
+            this.btnResize.TabIndex = 3;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
             // 
             // btnPrev
             // 
-            btnPrev.Enabled = false;
-            btnPrev.Location = new Point(548, 116);
-            btnPrev.Name = "btnPrev";
-            btnPrev.Size = new Size(102, 27);
-            btnPrev.TabIndex = 2;
-            btnPrev.Text = "Previous image";
-            btnPrev.UseVisualStyleBackColor = true;
-            btnPrev.Click += btnPrev_Click;
+            this.btnPrev.Enabled = false;
+            this.btnPrev.Location = new System.Drawing.Point(548, 116);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(102, 27);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.Text = "Previous image";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
             // 
             // btnNext
             // 
-            btnNext.Enabled = false;
-            btnNext.Location = new Point(548, 54);
-            btnNext.Name = "btnNext";
-            btnNext.Size = new Size(102, 27);
-            btnNext.TabIndex = 1;
-            btnNext.Text = "Next image";
-            btnNext.UseVisualStyleBackColor = true;
-            btnNext.Click += btnNext_Click;
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(548, 54);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(102, 27);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next image";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(62, 54);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(444, 286);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            this.pictureBox1.Location = new System.Drawing.Point(62, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 286);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // menuStrip1
             // 
-            menuStrip1.Items.AddRange(new ToolStripItem[] { menu1, menu2 });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 24);
-            menuStrip1.TabIndex = 8;
-            menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu1,
+            this.menu2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // menu1
             // 
-            menu1.Name = "menu1";
-            menu1.Size = new Size(84, 20);
-            menu1.Text = "Open Folder";
-            menu1.Click += menu1_Click_1;
+            this.menu1.Name = "menu1";
+            this.menu1.Size = new System.Drawing.Size(84, 20);
+            this.menu1.Text = "Open Folder";
+            this.menu1.Click += new System.EventHandler(this.menu1_Click_1);
             // 
             // menu2
             // 
-            menu2.Name = "menu2";
-            menu2.Size = new Size(69, 20);
-            menu2.Text = "Open File";
-            menu2.Click += menu2_Click_1;
+            this.menu2.Name = "menu2";
+            this.menu2.Size = new System.Drawing.Size(69, 20);
+            this.menu2.Text = "Open File";
+            this.menu2.Click += new System.EventHandler(this.menu2_Click_1);
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(panel1);
-            MainMenuStrip = menuStrip1;
-            Name = "Form1";
-            Text = "Form1";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)picture).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
@@ -297,11 +228,6 @@
         private ToolStripMenuItem menu2;
         private PictureBox pictureBox3;
         private Button btnEdit;
-        private Panel panel2;
-        private PictureBox pictureBox7;
-        private PictureBox pictureBox6;
-        private PictureBox pictureBox5;
-        private PictureBox pictureBox4;
         private ColorDialog colorDialog1;
     }
 }
